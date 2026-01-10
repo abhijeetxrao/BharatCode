33d16ab733b727e0c3d78667a8116a5ccac8ed82
Abhijeet Rao <abhijeetrao7800@gmail.com>
2026-01-10 23:26:03 +0530

    refactor createProblem controller for cleaner access check and export problemId function in judge0 library

--- backend/src/controllers/problem.controllers.js
+++ backend/src/controllers/problem.controllers.js
@@ -2,17 +2,18 @@ import problemRoute from "../routes/problem.route"
 
 export const createProblem=async(req,res)=>{
   const {title, description, difficulty, testCases, tags, examples, constraints, codeSnippets, referenceSolutions} = req.body;
-
-  if(req.user.role != "ADMIN"){
+  
+  if(req.user.role != "ADMIN")
     return res.status(403).json({message:"Access denied! Only admins can create problems."});
-  }
 
-  try {
-    
+  try {         
+
   } catch (error) {
-    
+
   }
 }
+
+
 
 export const getAllProblem=async(req,res)=>{}
 
@@ -23,3 +24,6 @@ export const getProblemById=async(req,res)=>{}
 export const deleteProblem=async(req,res)=>{}
 
 export const checkAllProblems=async(req,res)=>{}
+
+
+
--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -1,9 +1,9 @@
-const problemId =(language)=>{
+export const problemId =(language)=>{
   const languageMap = {
     'C': 50,
     'CPP': 54,
     'JAVA': 62,
     'PYTHON': 71,
   }
-  return languageMap[language]
 }
+return languageMap[language]
