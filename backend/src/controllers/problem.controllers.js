1399cfe980cf72c3597c3d5636efca412450e6bf
Abhijeet Rao <abhijeetrao7800@gmail.com>
2026-01-15 16:20:47 +0530

    add axios dependency and implement problem creation with reference solution validation in judge0 library

--- backend/src/controllers/problem.controllers.js
+++ backend/src/controllers/problem.controllers.js
@@ -1,4 +1,6 @@
+import {pollingBatch, problemId, submitBatch} from '../libs/judge0.lib.js'
 import problemRoute from "../routes/problem.route"
+
 
 export const createProblem=async(req,res)=>{
   const {title, description, difficulty, testCases, tags, examples, constraints, codeSnippets, referenceSolutions} = req.body;
@@ -6,7 +8,42 @@ export const createProblem=async(req,res)=>{
   if(req.user.role != "ADMIN")
     return res.status(403).json({message:"Access denied! Only admins can create problems."});
 
-  try {         
+  try {
+    
+    for(const [language, codeSnippets] of Object.entries(referenceSolutions)){
+      const languageId = problemId(language);
+      if(!languageId){
+        return res.status(400).json({message:`Unsupported programming language: ${language}`});
+      }
+
+      const  submission = testCases.map(({input, output})=>(
+        {"language_id": languageId,
+        "source_code": codeSnippets,
+        "stdin":input,
+        "expected_output": output
+        }
+      ))
+
+      const submissionResult = await submitBatch(submission); // [{token},{token},{token}]
+      const token = submissionResult.map(result => result.token);
+
+      const result = await pollingBatch(token);
+
+      for(let i =0; i<result.length; i++){
+        if(result[i].status.id !==3){
+          return res.status(400).json({message:`Reference solution failed for language: ${language} of testcase $(i+1)`});
+        }
+      }
+
+      const newProblem = await db.problem.create({
+        title, description, difficulty, testCases, tags, examples, constraints, codeSnippets, referenceSolutions
+      })
+      return res.json({newProblem})
+    }
+    
+
+
+
 
   } catch (error) {
 
--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -1,8 +1,44 @@
+import axios from 'axios'
+export const submitBatch = async(submission)=>{
+  const {data} = await axios.post(`${process.env.JUDGE0_API_URL}/submissions/?base64_encoded=false&wait=false`, submission)
+  return data;
+}
+
+const sleep =(ms)=>{
+  return new Promise(resolve=>setTimeout(resolve,ms));
+}
+
+export const pollingBatch = async(token)=>{
+  while(true){
+    const tokens = token.join(",");
+    const {data} = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`,{
+      params:{
+        tokens:tokens.join(","),
+        base64_encoded:false,
+      }
+    })
+
+    const result = data.submissions;
+    const isAllDone = result.every(r=>r.status.id!==1&&r.status.id!==2);
+    if(isAllDone){
+      return result;
+    }
+    sleep(1000);
+  }
+}
+
+
+
 export const problemId =(language)=>{
   const languageMap = {
     'C': 50,
     'CPP': 54,
     'JAVA': 62,
+    'JAVASCRIPT': 63,
   }
+  return languageMap[language.toUpperCase()];
 }
-return languageMap[language]
+
+
+
+
